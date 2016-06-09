a51dc1770868e7b0d34d4d55a560996b89d7110e
RJ Stroman <rj2sand@gmail.com>
2016-06-09 14:34:03 -0400

    HTTParty Food2fork

--- student-start/module3_1_assignment.rb
+++ student-start/module3_1_assignment.rb
@@ -1,5 +1,14 @@
 require 'httparty'
 
 class Recipe
+	include HTTParty
+
+	base_uri "http://www.food2fork.com/api"
+	default_params key: ENV["FOOD2FORK_KEY"]
+	format :json
+
+	def self.for keyword
+		get("/search", query: {q: keyword})["recipes"]
+	end
 
 end
